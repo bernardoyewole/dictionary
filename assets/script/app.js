45437aad157220326cbcd34e5982eba53ea0a55b
Bernard <oyebenard@gmail.com>
2023-12-24 14:16:27 -0600

    Word lookup functionality added

--- assets/script/app.js
+++ assets/script/app.js
@@ -2,13 +2,28 @@
 
 import { onEvent, select, selectAll, create, print } from "./utils.js";
 
+const input = select('input');
+const search = select('.search');
+const word = select('.word');
+const phoneticsText = select('.phonetics-text');
+const listen = select('.listen');
+const partOfSpeech = select('.part-of-speech');
+const meaning = select('.meaning');
+const suchAs = select('.such-as');
+
+let inputWord;
+
+onEvent('load', window, () => {
+    // input.value = '';
+});
+
 const options = {
     method: 'GET',
     mode: 'cors'
 }
 
-async function getRates() {
-    const URL = `https://api.dictionaryapi.dev/api/v2/entries/en/word`;
+async function getWordInfo() {
+    const URL = `https://api.dictionaryapi.dev/api/v2/entries/en/${inputWord}`;
 
     try {
         const response = await fetch(URL, options);
@@ -17,12 +32,62 @@ async function getRates() {
             throw new Error(`${response.statusText} (${response.status})`);
         }
 
-        const rates = await response.json();
-        console.log(rates);
+        const wordInfo = await response.json();
+        const meanings = wordInfo[0].meanings;
+        const phonetics = wordInfo[0].phonetics;
+
+        // console.log(phonetics);
+        setWord(inputWord, phonetics);
 
     } catch(error) {
-        console.log(error.message);
+        suchAs.innerText = `Sorry pal, we couldn't find the word you're looking for`;
     }
 }
 
-// getRates();
+let audio;
+
+function setWord(str, arr) {
+    word.innerText = str;
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].text !== '' && arr[i].audio !== '') {
+            phoneticsText.innerText = arr[i].text;
+            audio = new Audio(arr[i].audio);
+            break;
+        }
+    }
+}
+
+function isValid(str) {
+    if (str.match(/^[a-zA-Z]+$/) && str !== '') {
+        return true;
+    } else {
+        suchAs.innerText = 'Input is invalid';
+        suchAs.classList.add('invalid');
+        return false;
+    }
+}
+
+function searchFns() {
+    inputWord = input.value.trim();
+    getWordInfo();
+    suchAs.innerText = 'Such as: moon, nature, sunset...';
+    suchAs.classList.remove('invalid');
+    input.blur();
+}
+
+onEvent('click', search, () => {
+    if (isValid(input.value.trim())) {
+        searchFns();
+    }
+});
+
+onEvent('keydown', window, (event) => {
+    if (event.key === 'Enter') {
+        searchFns();
+    }
+})
+
+onEvent('click', listen, () => {
+    audio.play();
+});
